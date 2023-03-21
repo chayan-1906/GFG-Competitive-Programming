1056799ce959344c2d500d46c11e644ced545166
Padmanabha Das KIIT <padmanabhadas9647@gmail.com>
2023-03-21 21:11:11 +0530

    searchInSortedRotatedArray code corrected

--- Java/src/searching/SearchInSortedRotatedArray.java
+++ Java/src/searching/SearchInSortedRotatedArray.java
@@ -3,18 +3,20 @@ package searching;
 // https://practice.geeksforgeeks.org/batch/cp-47/track/DSASP-Searching/video/MTU5Nw%3D%3D
 public class SearchInSortedRotatedArray {
 
-	public static int searchInSortedRotatedArray(int[] array, int x) {
+	public static int searchInSortedRotatedArray(int[] array, int target) {
 		int left = 0, right = array.length - 1;
 		while (left <= right) {
 			int mid = left + (right - left) / 2;
-			if (array[ mid ] == x) return mid;
+			if (array[ mid ] == target) return mid;
 			else if (array[ left ] < array[ mid ]) {
-				// left half sorted
-				if (x >= array[ left ] && x < array[ mid ]) right = mid - 1;    // x lies between left to mid
+				// left array sorted
+				if (array[ left ] <= target && target < array[ mid ]) right = mid - 1;    // x lies between left to mid
+				else if (array[ left ] == target) return left;
 				else left = mid + 1;
 			} else {
-				// right half sorted
-				if (array[ mid ] < x && x <= array[ right ]) left = mid + 1;    // x lies between mid to right
+				// right array sorted
+				if (array[ mid ] < target && target <= array[ right ]) left = mid + 1;    // x lies between mid to right
+				else if (array[ right ] == target) return right;
 				else right = mid - 1;
 			}
 		}
@@ -32,5 +34,8 @@ public class SearchInSortedRotatedArray {
 		System.out.println ( searchInSortedRotatedArray ( new int[]{100, 500, 10, 20, 30, 40, 50}, 500 ) );  // 1
 		System.out.println ( searchInSortedRotatedArray ( new int[]{100, 200, 300, 400, 20, 30, 40}, 50 ) );  // -1
 		System.out.println ( searchInSortedRotatedArray ( new int[]{10, 20, 40, 60, 5, 8}, 5 ) );  // 4
+		System.out.println ( searchInSortedRotatedArray ( new int[]{10, 20, 40, 60, 5, 8}, 8 ) );  // 5
+		System.out.println ( searchInSortedRotatedArray ( new int[]{3, 1}, 1 ) );  // 1
+		System.out.println ( searchInSortedRotatedArray ( new int[]{5, 1, 2, 3, 4}, 1 ) );  // 1
 	}
 }
