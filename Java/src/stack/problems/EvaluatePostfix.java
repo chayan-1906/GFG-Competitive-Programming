6bda0f5a8337144c0407980137f80aad5c6c269b
Padmanabha Das <padmanabhadas9647@gmail.com>
2023-04-09 18:08:27 +0530

    EvaluatePostfix
    Signed-off-by: Padmanabha Das <padmanabhadas9647@gmail.com>

--- Java/src/stack/problems/EvaluatePostfix.java
+++ Java/src/stack/problems/EvaluatePostfix.java
@@ -1,10 +1,39 @@
 package stack.problems;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 // https://practice.geeksforgeeks.org/batch/cp-47/track/DSASP-Stack/problem/evaluation-of-postfix-expression1735
 public class EvaluatePostfix {
 
     public static int evaluatePostfix(String postfix) {
-        return 1;
+        Stack<String> stack = new Stack<>();
+        int result = 0;
+        for (int i = 0; i < postfix.length(); i++) {
+            char ch = postfix.charAt(i);
+            if (!isOperator(ch)) stack.push(String.valueOf(ch));
+            else {
+                int op1 = Integer.parseInt(stack.pop());
+                int op2 = Integer.parseInt(stack.pop());
+                if (ch == '+') result = op2 + op1;
+                else if (ch == '-') result = op2 - op1;
+                else if (ch == '*') result = op2 * op1;
+                else if (ch == '/') result = op2 / op1;
+                else if (ch == '^') result = (int) Math.pow(op2, op1);
+                stack.push(String.valueOf(result));
+            }
+        }
+        return result;
+    }
+
+    public static boolean isOperator(Character character) {
+        ArrayList<Character> characters = new ArrayList<>();
+        characters.add('+');
+        characters.add('-');
+        characters.add('*');
+        characters.add('/');
+        characters.add('^');
+        return characters.contains(character);
     }
 
     public static void main(String[] args) {
